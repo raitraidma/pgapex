16d7283835bb81b6105ef9b451bff6908f94b8d0
Nikolai Kopa <32339375+nikopa96@users.noreply.github.com>
2019-06-18 23:58:58 +0300

    Variables in config file have been replaced

--- pgapex/app/config.php
+++ pgapex/app/config.php
@@ -7,9 +7,9 @@ return [
       'driver' => env('driver', 'pgsql'),
       'host' => env('host', '127.0.0.1'),
       'port' => env('port', 5432),
-      'dbname' => env('dbname', 'pgapex'),
-      'username' => env('dbusername', 'pgapex_app'),
-      'password' => env('dbpassword', 'pgapex_app')
+      'dbname' => env('dbname', '#DB_DATABASE'),
+      'username' => env('dbusername', '#DB_APP_USER'),
+      'password' => env('dbpassword', '#DB_APP_USER_PASS')
     ]
   ]
 ];
